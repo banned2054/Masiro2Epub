abd4f32fbec52f4369569479698a3ca4840e708d
banned2054 <34962009+banned2054@users.noreply.github.com>
2023-02-15 19:54:45 +0800

    添加爬虫

---
+++ Masiro/lib/NetworkUnitTool.cs
@@ -0,0 +1,154 @@
+﻿using BrotliSharpLib;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Masiro.lib
+{
+    class NetworkUnitTool
+    {
+        static async Task<Token> GetToken()
+        {
+            var client  = new RestClient("https://masiro.me");
+            var request = new RestRequest("/admin/auth/login");
+
+            request.AddHeader("Accept",
+                              "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            request.AddHeader("Accept-Encoding", "gzip, deflate, br");
+            request.AddHeader("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7");
+            request.AddHeader("User-Agent",
+                              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36");
+
+            var response = await client.ExecuteAsync(request);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception("获取token失败！");
+            }
+            else
+            {
+                Console.WriteLine("token 获取成功");
+            }
+
+            var contentEncoding = response.Headers?.FirstOrDefault(h => h.Name != null &&
+                                                                        h.Name.Equals("Content-Encoding",
+                                                                            StringComparison.OrdinalIgnoreCase))
+                                         ?.Value
+                                         ?.ToString();
+            if (contentEncoding != null && contentEncoding.Contains("br"))
+            {
+                var compressedBytes = response.RawBytes;
+                if (compressedBytes != null)
+                {
+                    var uncompressedBytes  = Brotli.DecompressBuffer(compressedBytes, 0, compressedBytes.Length);
+                    var uncompressedString = System.Text.Encoding.UTF8.GetString(uncompressedBytes);
+                    response.Content = uncompressedString;
+                }
+            }
+
+            var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.LoadHtml(response.Content);
+            var token = htmlDoc.DocumentNode.SelectSingleNode("//input[@class='csrf']/@value")
+                              ?.GetAttributeValue("value", string.Empty);
+            if (token != null)
+                if (response.Cookies != null)
+                    return new Token(token, response.Cookies);
+            return new Token();
+        }
+
+        static async Task<string> LoginMasiro(Token nowToken, string userName, string password)
+        {
+            var client = new RestClient("https://masiro.me");
+            var loginData = new Dictionary<string, string>()
+                            {
+                                { "username", userName },
+                                { "password", password },
+                                { "remember", "1" },
+                                { "_token", nowToken.MyToken }
+                            };
+            var loginHeaders = new Dictionary<string, string>()
+                               {
+                                   {
+                                       "Accept",
+                                       "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+                                   },
+                                   {
+                                       "x-csrf-token", nowToken.MyToken
+                                   },
+
+                                   {
+                                       "x-requested-with", "XMLHttpRequest"
+                                   }
+                               };
+
+            var request = new RestRequest("/admin/auth/login", Method.Post);
+
+            var cookies = nowToken.MyCookie;
+
+            // Set Cookie header for login POST request
+            var cookieHeader = string.Join("; ", cookies.Select(c => $"{c.Name}={c.Value}"));
+            request.AddHeader("Cookie", cookieHeader);
+            foreach (var header in loginHeaders)
+            {
+                request.AddHeader(header.Key, header.Value);
+            }
+
+            foreach (var param in loginData)
+            {
+                request.AddParameter(param.Key, param.Value);
+            }
+
+            var loginPostResponse = await client.ExecuteAsync(request);
+            if (loginPostResponse.StatusCode != HttpStatusCode.OK)
+            {
+                return $"fail:{loginPostResponse.StatusCode}";
+            }
+
+            return "success";
+        }
+
+        static async Task<string> BookHtml(CookieCollection cookies, string subUrl)
+        {
+            var client          = new RestClient("https://masiro.me");
+            var cookieHeader    = string.Join("; ", cookies.Select(c => $"{c.Name}={c.Value}"));
+            var getNovelRequest = new RestRequest(subUrl);
+            getNovelRequest.AddHeader("Accept",
+                                      "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            getNovelRequest.AddHeader("Accept-Encoding", "gzip, deflate, br");
+            getNovelRequest.AddHeader("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7");
+            getNovelRequest.AddHeader("User-Agent",
+                                      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36");
+            getNovelRequest.AddHeader("Cookie", cookieHeader);
+
+            var novelResponse = await client.ExecuteAsync(getNovelRequest);
+
+            if (novelResponse.StatusCode != HttpStatusCode.OK)
+            {
+                return $"fail:{novelResponse.StatusDescription}";
+            }
+
+            return novelResponse.Content ?? "fail:No content";
+        }
+
+        class Token
+        {
+            public Token()
+            {
+                MyToken  = "";
+                MyCookie = new CookieCollection();
+            }
+
+            public Token(string token, CookieCollection cookie)
+            {
+                MyToken  = token;
+                MyCookie = cookie;
+            }
+
+            public string           MyToken  { get; set; }
+            public CookieCollection MyCookie { get; set; }
+        }
+    }
+}
--- Masiro/lib/StringUnitTool.cs
+++ Masiro/lib/StringUnitTool.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using OpenCCNET;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Transactions;
@@ -146,6 +149,98 @@ namespace Masiro.lib
             }
 
             return stringBuilder.ToString();
+        }
+
+        static List<Chapter> GetChapterList(string originText)
+        {
+            var chapterList = new List<Chapter>();
+            var htmlDoc     = new HtmlDocument();
+            htmlDoc.LoadHtml(originText);
+            var ulNode = htmlDoc.DocumentNode.SelectSingleNode("//ul[@class='chapter-ul']");
+            if (ulNode == null) return chapterList;
+            foreach (var liNode in ulNode.SelectNodes("li"))
+            {
+                if (liNode.SelectSingleNode(".//ul[@class='episode-ul']") == null)
+                {
+                    var bookTitle  = liNode.InnerText.Trim()[1..].Replace("&nbsp;", "");
+                    var newChapter = new Chapter(bookTitle);
+                    chapterList.Add(newChapter);
+                }
+                else
+                {
+                    var text = liNode.InnerHtml;
+
+                    if (!text.Contains("episode-ul")) continue;
+                    var nowEpisodeList = GetEpisodeList(text);
+                    var chapterLength  = chapterList.Count;
+                    if (chapterLength > 0)
+                    {
+                        chapterList[chapterLength - 1].SetEpisodeList(nowEpisodeList);
+                    }
+                }
+            }
+
+            return chapterList;
+        }
+
+        private static IEnumerable<Episode> GetEpisodeList(string originText)
+        {
+            var episodeList = new List<Episode>();
+            var htmlDoc     = new HtmlDocument();
+            htmlDoc.LoadHtml(originText);
+            var ulNode = htmlDoc.DocumentNode.SelectSingleNode("//ul[@class='episode-ul']");
+
+            if (ulNode == null) return episodeList;
+            episodeList = (from aNode in ulNode.SelectNodes("a")
+                           let href = aNode.GetAttributeValue("href", "")
+                           where href != null
+                           let aNodeInnerHtml = aNode.InnerHtml
+                           let title = GetEpisodeTitle(aNodeInnerHtml)
+                           where title != "" && href != ""
+                           select new Episode(title, href)).ToList();
+            return episodeList;
+        }
+
+        private static string GetEpisodeTitle(string originText)
+        {
+            ZhConverter.Initialize();
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(originText);
+            var liNode    = htmlDoc.DocumentNode.SelectSingleNode("li");
+            var spanNodes = liNode?.SelectNodes("span");
+            if (spanNodes is not { Count: > 0 }) return "";
+            var title = spanNodes[0].InnerText.Trim().Replace("&nbsp;", "");
+            title = title.ToHansFromHant();
+            return title;
+        }
+
+        class Episode
+        {
+            public Episode(string title, string subUrl)
+            {
+                Title  = title;
+                SubUrl = subUrl;
+            }
+
+            public string Title  { get; set; }
+            public string SubUrl { get; set; }
+        }
+
+        class Chapter
+        {
+            public Chapter(string bookTitle)
+            {
+                BookTitle   = bookTitle;
+                EpisodeList = new List<Episode>();
+            }
+
+            public void SetEpisodeList(IEnumerable<Episode> episodeList)
+            {
+                EpisodeList = new List<Episode>(episodeList);
+            }
+
+            public string        BookTitle   { get; set; }
+            public List<Episode> EpisodeList { get; set; }
         }
     }
 }
