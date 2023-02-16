5334a54de2286103e9608cb5e31c756235d88fd9
banned2054 <34962009+banned2054@users.noreply.github.com>
2023-02-16 16:03:19 +0800

    删去多余图片，添加主界面的ui

---
+++ Masiro/views/SearchPageUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Masiro.views
+{
+    /// <summary>
+    /// SearchPageUserControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class SearchPageUserControl : UserControl
+    {
+        public SearchPageUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
